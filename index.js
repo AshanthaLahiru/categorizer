45d382929536983ae4acb3b5e197cda4e22b9e9a
Ashantha Lahiru <ashantha.lahiru@gmail.com>
2018-03-29 23:39:19 +0530

    Add commands

--- index.js
+++ index.js
@@ -4,15 +4,11 @@ const Configuration = require('./lib/configuration')
 
 module.exports = (robot) => {
   robot.on('push', syncConfigurations)
+  commands(robot, 'category', addCategory)
+  robot.on(['issues.opened', 'pull_request.opened'], guideComment)
 
-  async function syncConfigurations(context) {
-
-    const content = await context.github.repos.getContent(context.repo({
-      path: '.github/categorizer.yml'
-    }))
-    const config = yaml.safeLoad(Buffer.from(content.data.content, 'base64').toString())
-
-    context.log(config)
+  async function syncConfigurations (context) {
+    const config = await getConfiguration(context)
 
     const configurationsModified = context.payload.commits.find(commit => {
       return commit.added.includes('.github/categorizer.yml') ||
@@ -22,5 +18,51 @@ module.exports = (robot) => {
     if (configurationsModified) {
       return new Configuration(context.github, context.repo(), config).sync()
     }
+  }
+
+  async function addCategory (context, command) {
+    let asyncActivities = []
+
+    const config = await getConfiguration(context)
+
+    let subCategory = config.categories.find(category => {
+      return category.keyword === command.arguments.split('-')[1]
+    })
+
+    asyncActivities.push(await context.github.issues.deleteComment(context.issue({id: context.payload.comment.id})))
+
+    if (subCategory) {
+      asyncActivities.push(await context.github.issues.addLabels(context.issue({labels: [subCategory.label.name]})))
+    } else {
+      let bodyMsg = 'There is no such a category :worried:, please use a following category. <br/><br/>'
+
+      for (let x = 0; x < config.categories.length; x++) {
+        bodyMsg = bodyMsg.concat('`/concat-' + config.categories[x].keyword + '-` : ' + config.categories[x].label.description + '<br/>')
+      }
+
+      asyncActivities.push(await context.github.issues.createComment(context.issue({body: bodyMsg})))
+    }
+
+    Promise.all(asyncActivities)
+  }
+
+  async function guideComment (context) {
+    const config = await getConfiguration(context)
+
+    let bodyMsg = 'It would be helpful if you can give a category from following categories for this issue. <br/><br/>'
+
+    for (let x = 0; x < config.categories.length; x++) {
+      bodyMsg = bodyMsg.concat('`/category-' + config.categories[x].keyword + '-` : ' + config.categories[x].label.description + '<br/>')
+    }
+
+    await context.github.issues.createComment(context.issue({body: bodyMsg}))
+  }
+
+  async function getConfiguration (context) {
+    const content = await context.github.repos.getContent(context.repo({
+      path: '.github/categorizer.yml'
+    }))
+
+    return yaml.safeLoad(Buffer.from(content.data.content, 'base64').toString())
   }
 }
