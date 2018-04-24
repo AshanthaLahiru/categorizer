5b02a1ca1251e197e9d2ba557a0ec8de5cbd7322
Ashantha Lahiru <ashantha.lahiru@gmail.com>
2018-04-24 07:31:57 +0530

    Add context.config() instead of Configuration

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const commands = require('probot-commands')
 const Core = require('./lib/core')
 
 module.exports = (robot) => {
-  robot.on('push', Core.syncConfigurations)
   commands(robot, 'cat', Core.addCategory)
   robot.on(['issues.opened', 'pull_request.opened'], Core.guideComment)
 }
